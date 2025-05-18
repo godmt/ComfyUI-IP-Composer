7968b133ff0804b8c1f5ae9cbf89bc933559fd31
godmt <55956803+godmt@users.noreply.github.com>
2025-05-18 18:51:53 +0900

    v1 release, concept format change

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 ComfyUI implementation of IP-Composer (https://ip-composer.github.io/IP-Composer/)
 ![image](https://github.com/user-attachments/assets/21752669-f940-42a4-a498-b0909f6739e2)
 
-## Collection of Concept Projection Matrices
-https://huggingface.co/ReadyON/IP-Composer-projection-collection
+## Collection of Concept Subspaces
+model directory: `models\ip_concept_subspaces`  
+https://huggingface.co/ReadyON/IP-Composer-Concept-Subspaces
--- nodes.py
+++ nodes.py
@@ -7,7 +7,7 @@ import open_clip
 import folder_paths
 from pathlib import Path
 
-MODEL_DIR_NAME = "ip_concept_matrices"
+MODEL_DIR_NAME = "ip_concept_subspaces"
 models_dir = Path(__file__).parent.parent.parent / "models"
 root_path = models_dir.resolve() / MODEL_DIR_NAME
 if not root_path.exists():
@@ -49,7 +49,7 @@ class IPLoadCLIPVision:
         },)
 
 
-class IPConceptMatrix:
+class IPConceptSubspace:
     @classmethod
     def INPUT_TYPES(cls):
         return {"required": {
@@ -58,17 +58,17 @@ class IPConceptMatrix:
             "rank": ("INT", {"default": 30, "min": 1, "max": 1024}),
             "batch_size": ("INT", {"default": 100, "min": 1, "max": 512})
         }}
-    RETURN_TYPES = ("TENSOR",)
-    RETURN_NAMES = ("concept_matrix",)
+    RETURN_TYPES = ("CONCEPT_SUBSPACE",)
+    RETURN_NAMES = ("concept_subspace",)
     CATEGORY = "ip_composer"
-    TITLE = "IP-Comp Concept Matrix"
+    TITLE = "IP-Comp Concept Subspace"
     FUNCTION = "execute"
 
     def execute(self, open_clip_model, descriptions, rank, batch_size):
         device = open_clip_model["device"]
         model = open_clip_model["model"]
         tokenizer = open_clip_model["tokenizer"]
-        # 1. 行リストを作成
+        # 1. create list of concept descriptions
         lines = [l.strip() for l in descriptions.split("\n") if l.strip()]
         # 2. encode concept descriptions
         with torch.no_grad():
@@ -78,6 +78,7 @@ class IPConceptMatrix:
                 batch_desc = lines[i:i + batch_size]
                 texts = tokenizer(batch_desc).to(device)
                 batch_embeddings = model.encode_text(texts)
+                # TODO is it necessary to convert to numpy?
                 batch_embeddings = batch_embeddings.detach().cpu().numpy()
                 embs.append(batch_embeddings)
                 del texts, batch_embeddings
@@ -85,53 +86,60 @@ class IPConceptMatrix:
             embs = np.vstack(embs)
             # Perform SVD on the combined matrix
             _, _, v = np.linalg.svd(embs, full_matrices=False)
-        # Select the top `rank` singular vectors to construct the projection matrix for the concept direction
-        v = v[:rank]
-        # project via (Vᵀ V) ≈ identity on subspace
-        P = v.T @ v  # (1024, 1024)
-        P = torch.from_numpy(P)
-        return (P,)
+        # specify the recommended top `rank` singular vectors to construct the concept subspace
+        concept_subspace = {
+            "metadata": {"rank": str(rank), "clip_vision_model": open_clip_model["hf_name"]},
+            "concept_subspace": torch.from_numpy(v),
+        }
+        return (concept_subspace,)
 
 
-class IPSaveConceptMatrix:
+class IPSaveConceptSubspace:
     @classmethod
     def INPUT_TYPES(cls):
         return {"required": {
-            "open_clip_model": ("OPEN_CLIP",),
-            "concept_matrix": ("TENSOR",),
+            "concept_subspace": ("CONCEPT_SUBSPACE",),
             "concept_name": ("STRING", {"default": "concept"})
         }}
     RETURN_TYPES = ()
     CATEGORY = "ip_composer"
-    TITLE = "IP-Comp Save Concept Matrix"
+    TITLE = "IP-Comp Save Concept Subspace"
     FUNCTION = "execute"
     OUTPUT_NODE = True
 
-    def execute(self, open_clip_model, concept_matrix, concept_name):
-        meta = {"clip_vision_model": open_clip_model["hf_name"]}
+    def execute(self, concept_subspace, concept_name):
         file_path = str(root_path / f"{concept_name}.safetensors")
-        st.save_file({"concept_matrix": concept_matrix.cpu()}, file_path, metadata=meta)
+        st.save_file({"concept_subspace": concept_subspace["concept_subspace"].cpu()}, file_path, metadata=concept_subspace["metadata"])
         return ()
 
 
-class IPLoadConceptMatrix:
+class IPLoadConceptSubspace:
     @classmethod
     def INPUT_TYPES(cls):
-        matrix_names = folder_paths.get_filename_list(MODEL_DIR_NAME)
+        concept_names = folder_paths.get_filename_list(MODEL_DIR_NAME)
         return {"required": {
-            "matrix_name": (matrix_names,),
+            "concept_name": (concept_names,),
         }}
-    RETURN_TYPES = ("TENSOR",)
-    RETURN_NAMES = ("concept_matrix",)
+    RETURN_TYPES = ("CONCEPT_SUBSPACE",)
+    RETURN_NAMES = ("concept_subspace",)
     CATEGORY = "ip_composer"
-    TITLE = "IP-Comp Load Concept Matrix"
+    TITLE = "IP-Comp Load Concept Subspace"
     FUNCTION = "execute"
 
-    def execute(self, matrix_name):
-        file_path = folder_paths.get_full_path(MODEL_DIR_NAME, matrix_name)
-        data = st.load_file(file_path)
-        P = data["concept_matrix"]
-        return (P,)
+    def execute(self, concept_name):
+        file_path = folder_paths.get_full_path(MODEL_DIR_NAME, concept_name)
+        with st.safe_open(file_path, framework="pt", device="cpu") as f:
+            # Load the metadata
+            metadata = f.metadata()
+            # Load the tensor
+            concept_subspace = f.get_tensor("concept_subspace")
+        
+        concept_subspace = {
+            "metadata": metadata,
+            "concept_subspace": concept_subspace,
+        }
+
+        return (concept_subspace,)
 
 
 class IPConceptMerge:
@@ -140,17 +148,19 @@ class IPConceptMerge:
         return {"required": {
             "ref_embed": ("EMBEDS",),
             "concept_embed": ("EMBEDS",),
-            "concept_matrix": ("TENSOR",)
+            "concept_subspace": ("CONCEPT_SUBSPACE",),
+            "rank": ("INT", {"default": -1, "min": -1, "max": 1024})
         }}
     RETURN_TYPES = ("EMBEDS",)  # merged embeddings
     CATEGORY = "ip_composer"
     TITLE = "IP-Comp Concept Merge"
     FUNCTION = "execute"
 
-    def execute(self, ref_embed, concept_embed, concept_matrix):
-        P = concept_matrix
-        ref_embed = ref_embed
-        concept_embed = concept_embed
+    def execute(self, ref_embed, concept_embed, concept_subspace, rank):
+        if rank == -1:
+            rank = int(concept_subspace["metadata"]["rank"])
+        P = concept_subspace["concept_subspace"][:rank, :]
+        P = P.T @ P  # concept projection matrix, (1024, 1024)
         # merge concept        
         e_mix = ref_embed - ref_embed @ P + concept_embed @ P
         return (e_mix,)
@@ -158,8 +168,8 @@ class IPConceptMerge:
 
 NODE_CLASS_MAPPINGS = {
     "IPCompLoadOpenCLIP": IPLoadCLIPVision,
-    "IPCompConceptSubspace": IPConceptMatrix,
+    "IPCompConceptSubspace": IPConceptSubspace,
     "IPCompConceptMerge": IPConceptMerge,
-    "IPSaveConceptMatrix": IPSaveConceptMatrix,
-    "IPLoadConceptMatrix": IPLoadConceptMatrix
+    "IPSaveConceptSubspace": IPSaveConceptSubspace,
+    "IPLoadConceptSubspace": IPLoadConceptSubspace
 }
